61a89d32a379d880f1f21743f15b19fa1f582fbe
polokoti <63522828+polokoti@users.noreply.github.com>
2020-09-04 10:37:23 +0700

    Add Player

---
+++ C5-Magnetize/Assets/Script/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb2D;
+    public float moveSpeed = 5f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2D = this.gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move Object
+        rb2D.velocity = -transform.up * moveSpeed;
+    }
+}
