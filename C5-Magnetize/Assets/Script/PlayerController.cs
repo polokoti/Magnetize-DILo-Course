8e23d665623106b1d87369e931df321b7ce3f477
polokoti <63522828+polokoti@users.noreply.github.com>
2020-09-04 18:50:40 +0700

    Add Tower

--- C5-Magnetize/Assets/Script/PlayerController.cs
+++ C5-Magnetize/Assets/Script/PlayerController.cs
@@ -6,6 +6,11 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb2D;
     public float moveSpeed = 5f;
+    public float pullForce = 100f;
+    public float rotateSpeed = 360f;
+    private GameObject closestTower;
+    private GameObject hookedTower;
+    private bool isPulled = false;
 
 
     // Start is called before the first frame update
@@ -19,6 +24,37 @@ public class PlayerController : MonoBehaviour
     {
         // Move Object
         rb2D.velocity = -transform.up * moveSpeed;
+
+        if(Input.GetKey(KeyCode.Z) && !isPulled)
+        {
+            if (closestTower != null && hookedTower == null)
+            {
+                hookedTower = closestTower;
+            }
+
+            if (hookedTower)
+            {
+                float distance = Vector2.Distance(transform.position, hookedTower.transform.position);
+
+                //Gravitation toward tower
+                Vector3 pullDirection = (hookedTower.transform.position - transform.position).normalized;
+                float newPullForce = Mathf.Clamp(pullForce / distance, 20, 50);
+                rb2D.AddForce(pullDirection * newPullForce);
+                isPulled = true;
+
+                //Angular Velocity
+                rb2D.angularVelocity = -rotateSpeed / distance;
+                isPulled = true;
+
+            }
+        }
+
+        if(Input.GetKeyUp(KeyCode.Z))
+        {
+            isPulled = false;
+            hookedTower = null;
+        }
+
     }
 
     public void OnCollisionEnter2D(Collision2D collision)
@@ -35,6 +71,31 @@ public class PlayerController : MonoBehaviour
         if(collision.gameObject.tag == "Goal")
         {
             Debug.Log("LevelClear!");
+        }
+    }
+
+    public void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Tower")
+        {
+            closestTower = collision.gameObject;
+
+            // Change tower color to green as indicator
+            collision.gameObject.GetComponent<SpriteRenderer>().color = Color.green;
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if (isPulled) return;
+
+        if (collision.gameObject.tag == "Tower")
+        {
+            closestTower = null;
+            hookedTower = null;
+
+            // Change tower color back to white
+            collision.gameObject.GetComponent<SpriteRenderer>().color = Color.white;
         }
     }
 }
